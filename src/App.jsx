25a7340286981e31ee102c20be24575014625e30
Temiloluwa <temiloluwaOgunti8@gmail.com>
2024-08-08 20:08:19 +0100

    Added a .env file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Chat from "./pages/chat/chat";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 
-// google api key - AIzaSyBE16gMqR6XG9skX8x8JZVzdm-aoDOO5V0
-// clientId - 531103568928-phrlu71k9e07e1775nec9otbdiuoje60.apps.googleusercontent.com
-// client secret - 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
 
 function App() {
   return (
